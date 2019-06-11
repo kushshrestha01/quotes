4147eba8975b7c7cd289de142f1449099d70c32a
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-06-11 13:06:43 -0700

    lab completed

--- src/main/java/quotes/Quote.java
+++ src/main/java/quotes/Quote.java
@@ -3,10 +3,14 @@ package quotes;
 public class Quote {
     String author;
     String text;
+    String[] tags = new String[1];
+    String likes;
 
-    Quote(String author, String quotes){
+    Quote(String author, String quotes, String[] tags, String likes){
         this.author = author;
         this.text = quotes;
+        this.tags = tags;
+        this.likes = likes;
     }
 
     public String toString() {
